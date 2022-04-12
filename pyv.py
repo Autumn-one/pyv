e99043c921ede4a66813f542cabc3fa06bef14e1
Autumn-one <718879459@qq.com>
2022-04-12 13:37:20 +0800

    提交颜色打印

--- pyv.py
+++ pyv.py
@@ -1,6 +1,9 @@
 import os
 import sys
 import re
+from colorama import init, Fore, Back, Style
+
+init(autoreset=True) # 初始化 windows必须
 args = sys.argv
 if len(args) >= 2:
     arg = args[1]
@@ -8,6 +11,9 @@ if len(args) >= 2:
         os.system("py -0")
     elif re.match(r"[23]\.\d+", arg):
         os.system(f"setx PY_PYTHON {args[1]}")
-        print(f"python版本设置完成, 重启命令行生效! 版本号: {args[1]}")
+        print(Fore.GREEN + f"python版本设置完成, 重启命令行生效! 版本号: {args[1]}")
     else:
-        print("无法识别的指令")
+        print(Fore.RED + "无法识别的指令!")
+
+
+
